90ea32e9f8404653d57ae62295625cfda7afbda1
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2022-08-04 17:50:49 +0300

    Added configuration

--- Api/ConfigInterface.php
+++ Api/ConfigInterface.php
@@ -8,7 +8,25 @@ declare(strict_types=1);
 
 namespace Hryvinskyi\PageSpeedCss\Api;
 
+use Magento\Store\Model\ScopeInterface;
+
 interface ConfigInterface
 {
+    /**
+     * Returns is enabled move to bottom css.
+     *
+     * @param $scopeCode
+     * @param string $scopeType
+     * @return bool
+     */
+    public function isEnableMoveToBottom($scopeCode = null, string $scopeType = ScopeInterface::SCOPE_STORE): bool;
 
+    /**
+     * Returns is enabled merge inline css.
+     *
+     * @param $scopeCode
+     * @param string $scopeType
+     * @return bool
+     */
+    public function isEnableMergeInlineCss($scopeCode = null, string $scopeType = ScopeInterface::SCOPE_STORE): bool;
 }
--- Model/Config.php
+++ Model/Config.php
@@ -9,15 +9,42 @@ declare(strict_types=1);
 namespace Hryvinskyi\PageSpeedCss\Model;
 
 use Hryvinskyi\PageSpeedCss\Api\ConfigInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
 class Config implements ConfigInterface
 {
+    public const XML_PATH_ENABLE_MOVE_TO_BOTTOM = 'hryvinskyi_pagespeed/css/enable_move_to_bottom';
+    public const XML_PATH_ENABLE_MERGE_INLINE_CSS = 'hryvinskyi_pagespeed/css/enable_merge_inline_css';
+
+    private ScopeConfigInterface $scopeConfig;
+
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
     /**
      * @inheritdoc
      */
     public function isOnlyDeferred($scopeCode = null, string $scopeType = ScopeInterface::SCOPE_STORE): bool
     {
         return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEnableMergeInlineCss($scopeCode = null, string $scopeType = ScopeInterface::SCOPE_STORE): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_ENABLE_MERGE_INLINE_CSS, $scopeCode, $scopeType);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEnableMoveToBottom($scopeCode = null, string $scopeType = ScopeInterface::SCOPE_STORE): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_ENABLE_MOVE_TO_BOTTOM, $scopeCode, $scopeType);
     }
 }
